d9ff85deb36b46fc86695d420cb4cb368042437e
jonatas224 <portelajonatas224@gmail.com>
2024-06-06 19:59:27 -0300

    adicione arquivo

--- aula 9/script.js
+++ aula 9/script.js
@@ -10,3 +10,9 @@ novoParagrafo.textContent = 'meu paragrafo'
 container.appendChild(novoParagrafo)
 
 container.innerHTML += `<p class='paragrafo2'>Meu paragrafo com InnerHTML</p>`
+
+let novoButton = document.createElement('button')
+novoButton.textContent = 'click'
+novoButton.id = 'buttonClick'
+
+container.appendChild(novoButton)
