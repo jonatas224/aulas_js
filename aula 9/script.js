a79efd4a8d7d32c18f9087eb170b432c4eeee715
jonatas224 <portelajonatas224@gmail.com>
2024-06-06 20:27:03 -0300

    adicione arquivo

--- aula 9/script.js
+++ aula 9/script.js
@@ -1,18 +1,30 @@
 // criando elementos
 
-let container = document.querySelector('.container')
-console.log(container)
+let container = document.querySelector(".container");
+console.log(container);
 
-let novoParagrafo = document.createElement('p')
-novoParagrafo.className = 'paragrafo'
-novoParagrafo.textContent = 'meu paragrafo'
+let novoParagrafo = document.createElement("p");
+novoParagrafo.className = "paragrafo";
+novoParagrafo.textContent = "meu paragrafo";
 
-container.appendChild(novoParagrafo)
+container.appendChild(novoParagrafo);
 
-container.innerHTML += `<p class='paragrafo2'>Meu paragrafo com InnerHTML</p>`
+container.innerHTML += `<p class='paragrafo2'>Meu paragrafo com InnerHTML</p>`;
 
-let novoButton = document.createElement('button')
-novoButton.textContent = 'click'
-novoButton.id = 'buttonClick'
+let novoButton = document.createElement("button");
+novoButton.textContent = "click";
+novoButton.id = "buttonClick";
+novoButton.type = "button";
 
-container.appendChild(novoButton)
+container.appendChild(novoButton);
+
+// novoButton.addEventListener("click",function(){
+//     alert("oi");
+// });
+
+let acessButton = document.getElementById("buttonClick");
+
+acessButton.addEventListener("click", function () {
+  alert("oi");
+  container.style.background = "red";
+});
