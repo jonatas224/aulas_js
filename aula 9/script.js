4618037b9296390b63cfaaa1f696cf428803fcdc
jonatas224 <portelajonatas224@gmail.com>
2024-06-06 19:47:40 -0300

    adicione arquivo

---
+++ aula 9/script.js
@@ -0,0 +1,12 @@
+// criando elementos
+
+let container = document.querySelector('.container')
+console.log(container)
+
+let novoElemento = document.createElement('p')
+novoElemento.className = 'paragrafo'
+novoElemento.textContent = 'meu paragrafo'
+
+container.appendChild(novoElemento)
+
+container.innerHTML = `<p class='paragrafo'>Meu paragrafo com InnerHTML</p>`
