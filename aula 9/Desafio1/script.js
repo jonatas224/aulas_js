b26324ed8fb1a968dce14f4e7343c22e9bbe3eac
jonatas224 <portelajonatas224@gmail.com>
2024-06-06 21:21:10 -0300

    adicione arquivo

--- aula 9/Desafio1/script.js
+++ aula 9/Desafio1/script.js
@@ -5,9 +5,31 @@ let lista = document.getElementById("listaDeTarefas");
 buttonAdicionar.addEventListener("click", function () {
   console.log(novaTarefa.value);
 
+  if (novaTarefa.value !== "") {
+    let li = document.createElement("li");
+    li.textContent = novaTarefa.value;
 
-  
-  let li = document.createElement("li");
-  li.textContent = novaTarefa.value;
-  lista.appendChild(li);
+    let inputcheck = document.createElement("input");
+    inputcheck.type = "checkbox"
+
+    inputcheck.addEventListener('click',()=>{
+console.log(inputcheck.checked)
+
+if(inputcheck.checked === true){
+li.className = 'feito'
+}else{
+    li.className = ''
+}
+    })
+
+    let buttonDeleter = document.createElement("button");
+    buttonDeleter.textContent = "excluir tarefa";
+    buttonDeleter.addEventListener("click", function () {
+      lista.removeChild(li);
+    });
+
+    li.appendChild(buttonDeleter);
+    lista.appendChild(li);
+    novaTarefa.value = "";
+  }
 });
